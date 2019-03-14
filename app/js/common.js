1984b87a815cbf0e8b11f7dea0ffbda4468486d3
Ihor Holohots <igor.gologots@hyuna.bb>
2019-03-14 18:45:31 +0200

    Added slide section

--- app/js/common.js
+++ app/js/common.js
@@ -1,21 +1,9 @@
 $(document).ready(function() {
-	/* Parallax 
-	$('.parallax-window').parallax({imageSrc: 'img/background.jpg'}); */
-
-	/* Resize
-	function heightDetect(){
-		$(".parallax-window").css("height", $(window).height());	
-	}
-	heightDetect();
-	$(window).resize(function(){
-		heightDetect();
-	});
-	 */
-	 
-	 /* Burger */
+	/* Burger */
     $(".toggle_mnu").click(function() {
         $(".sandwich").toggleClass("active");
     });
+
     /* Burger. Hover */
     $(".top_mnu ul a").click(function() {
         $(".top_mnu").fadeOut(600);
@@ -35,4 +23,44 @@ $(document).ready(function() {
             $(".top_mnu li a").addClass("fadeInUp animated");
         };
     });
+
+    $('.carousel').carousel({
+      interval: 8000
+    })
+
+    /*Scrool animation*/
+    $(window).scroll(function() {
+        $('.mov').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 500) {
+                $(this).addClass('fadeIn');
+                $(this).css('opacity', '1');
+            }
+        });
+    });
+
+    /* Easy slide 
+    $("header .top_mnu ul a").mPageScroll2id();*/
+
+    /* Speed scrool 
+    function wheel(event) {
+        var delta = 0;
+        if (event.wheelDelta) {(delta = event.wheelDelta / 120);}
+        else if (event.detail) {(delta = -event.detail / 3);}
+
+        handle(delta);
+        if (event.preventDefault) {(event.preventDefault());}
+        event.returnValue = false;
+    }
+    function handle(delta) {
+        var time = 200;
+        var distance = 500;
+
+        $('html, body').stop().animate({
+            scrollTop: $(window).scrollTop() - (distance * delta)
+        }, time );
+    }
+    if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
+    window.onmousewheel = document.onmousewheel = wheel;*/
 });
