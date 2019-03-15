f6b53ae80f05e357c744c7c7b7d70c2f5514d302
Ihor Holohots <igor.gologots@hyuna.bb>
2019-03-15 18:41:49 +0200

    Added free to join section

--- app/js/common.js
+++ app/js/common.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 
     $('.carousel').carousel({
       interval: 8000
-    })
+  })
 
     /*Scrool animation*/
     $(window).scroll(function() {
@@ -39,6 +39,26 @@ $(document).ready(function() {
             }
         });
     });
+
+    /* Video button */
+    $('#play-video').on('click', function(e){
+      e.preventDefault();
+      $('#video-overlay').addClass('open');
+      $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+  });
+
+    $('.video-overlay, .video-overlay-close').on('click', function(e){
+      e.preventDefault();
+      close_video();
+  });
+
+    $(document).keyup(function(e){
+      if(e.keyCode === 27) { close_video(); }
+  });
+
+    function close_video() {
+      $('.video-overlay.open').removeClass('open').find('iframe').remove();
+  };
 
     /* Easy slide 
     $("header .top_mnu ul a").mPageScroll2id();*/
